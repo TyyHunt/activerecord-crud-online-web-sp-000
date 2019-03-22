e231c23e54635f384db1d7db46e1305af47eafa9
Tyler Hunt <tyhunt04@gmail.com>
2019-03-22 22:18:58 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -47,8 +47,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  num = Movie.first.id
-  Movie.find(num)
+  Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
